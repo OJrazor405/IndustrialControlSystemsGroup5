fa960c8500a564ff45f965d0fe08b1f5b7118de8
Sander3214 <sander.overbo@hotmail.com>
2023-11-02 15:31:11 +0100

    haslla

--- ProjectIndustrialControlSystems/UserControls/ucHome.cs
+++ ProjectIndustrialControlSystems/UserControls/ucHome.cs
@@ -19,9 +19,11 @@ namespace ProjectIndustrialControlSystems.UserControls
         LogClient logClient;
         AlarmEntity alarm;
         SensorData sensorData;
-        AlarmEntity alarmEntity;
+        AlarmEntity alarmEntityLow;
+        AlarmEntity alarmEntityHigh;
         bool tempMan = false;
-        bool alarmSet = false;
+        bool alarmSetHigh = false;
+        bool alarmSetLow = false;
 
         public ucHome()
         {
@@ -42,7 +44,7 @@ namespace ProjectIndustrialControlSystems.UserControls
             }
         }
 
-        private void updateTbPoolTemp()
+        private async void updateTbPoolTemp()
         {
             if (!tempMan)//auto, får verdier fra Rpi
             {
@@ -50,63 +52,77 @@ namespace ProjectIndustrialControlSystems.UserControls
                 txtPoolTemp.Text = tbPoolTemp.Value.ToString();
                 if (tbPoolTemp.Value <= tbSetPoolTemp.Value - 5)
                 {
-                    if (!alarmSet)
+                    if (!alarmSetLow)
                     {
-                        alarmEntity = new AlarmEntity("Temperatur basseng lav", false, DateTime.Now, true, Color.Red);
-                        logClient.AddAlarmEntity(alarmEntity);
-                        alarmSet = true;
+                        alarmEntityLow = new AlarmEntity("Temperatur basseng lav", false, DateTime.Now, true, Color.Red);
+                        logClient.AddAlarmEntity(alarmEntityLow);
+                        alarmSetLow = true;
                     }
                 }
-                else if (tbPoolTemp.Value > tbSetPoolTemp.Value - 5)
+                else if (tbPoolTemp.Value > tbSetPoolTemp.Value - 5 && alarmSetLow == true)
                 {
-                    alarmSet = false;
+                    alarmSetLow = false;
+                    alarmEntityLow.State = false;
+                    alarmEntityLow.AlarmColor = Color.Yellow.ToArgb().ToString();
+                    await logClient.UpdateAlarm(alarmEntityLow);
                 }
                 else if (tbPoolTemp.Value >= tbSetPoolTemp.Value + 5)
                 {
-                    if (!alarmSet)
+                    if (!alarmSetHigh)
                     {
-                        alarmEntity = new AlarmEntity("Temperatur basseng høy", false, DateTime.Now, true, Color.Red);
-                        logClient.AddAlarmEntity(alarmEntity);
-                        alarmSet = true;
+                        alarmEntityHigh = new AlarmEntity("Temperatur basseng høy", false, DateTime.Now, true, Color.Red);
+                        logClient.AddAlarmEntity(alarmEntityHigh);
+                        alarmSetHigh = true;
                     }
                 }
-                else if (tbPoolTemp.Value < tbSetPoolTemp.Value + 5)
+                else if (tbPoolTemp.Value < tbSetPoolTemp.Value + 5 && alarmSetHigh == true)
                 {
-                       alarmSet = false;
+                    alarmSetHigh = false;
+                    alarmEntityHigh.State = false;
+                    alarmEntityHigh.AlarmColor = Color.Yellow.ToArgb().ToString();
+                    await logClient.UpdateAlarm(alarmEntityHigh);
+
                 }
             }
         }
 
-        private void tbPoolTemp_Scroll(object sender, EventArgs e)
+        private async void tbPoolTemp_Scroll(object sender, EventArgs e)
         {
             if (tempMan)
             {
                 txtPoolTemp.Text = tbPoolTemp.Value.ToString("0");
                 if (tbPoolTemp.Value <= tbSetPoolTemp.Value - 5)
                 {
-                    if (!alarmSet)
+                    if (!alarmSetLow)
                     {
-                        alarmEntity = new AlarmEntity("Temperatur basseng lav", false, DateTime.Now, true, Color.Red);
-                        logClient.AddAlarmEntity(alarmEntity);
-                        alarmSet = true;
+                        alarmEntityLow = new AlarmEntity("Temperatur basseng lav", false, DateTime.Now, true, Color.Red);
+                        logClient.AddAlarmEntity(alarmEntityLow);
+                        alarmSetLow = true;
                     }
                 }
-                else if (tbPoolTemp.Value > tbSetPoolTemp.Value - 5)
+                else if (tbPoolTemp.Value > tbSetPoolTemp.Value - 5 && alarmSetLow == true)
                 {
-                    alarmSet = false;
+                    alarmSetLow = false;
+                    alarmEntityLow.State = false;
+                    alarmEntityLow.AlarmColor = Color.Yellow.ToArgb().ToString();
+                    await logClient.UpdateAlarm(alarmEntityLow);
                 }
                 else if (tbPoolTemp.Value >= tbSetPoolTemp.Value + 5)
                 {
-                    if (!alarmSet)
+                    if (!alarmSetHigh)
                     {
-                        alarmEntity = new AlarmEntity("Temperatur basseng høy", false, DateTime.Now, true, Color.Red);
-                        logClient.AddAlarmEntity(alarmEntity);
-                        alarmSet = true;
+                        alarmEntityHigh = new AlarmEntity("Temperatur basseng høy", false, DateTime.Now, true, Color.Red);
+                        logClient.AddAlarmEntity(alarmEntityHigh);
+                        alarmSetHigh = true;
                     }
                 }
-                else if (tbPoolTemp.Value < tbSetPoolTemp.Value + 5)
+                else if (tbPoolTemp.Value < tbSetPoolTemp.Value + 5 && alarmSetHigh == true)
                 {
-                    alarmSet = false;
+                    alarmSetHigh = false;
+                    alarmEntityHigh.State = false;
+                    alarmEntityHigh.AlarmColor = Color.Yellow.ToArgb().ToString();
+                    await logClient.UpdateAlarm(alarmEntityHigh);
+
                 }
             }          
 
